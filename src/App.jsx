87c83287489207c9faf17a1a386e38d181b6d493
ayequill <ayequillgh@gmail.com>
2023-01-09 17:40:32 +0000

    formating

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import Info from './components/Info'
-import About from './components/About'
-import Interest from './components/Interest'
-import Footer from './components/Footer'
+import { useState } from "react";
+import Info from "./components/Info";
+import About from "./components/About";
+import Interest from "./components/Interest";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="container">
@@ -15,7 +14,7 @@ function App() {
       <Interest />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Interest.jsx
+++ src/components/Interest.jsx
@@ -1,8 +1,11 @@
-export default function Interest () {
-    return (
-        <div className="interest">
-            <h3>Interests</h3>
-            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
-        </div>
-    )
+export default function Interest() {
+  return (
+    <div className="interest">
+      <h3>Interests</h3>
+      <p>
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+      </p>
+    </div>
+  );
 }
