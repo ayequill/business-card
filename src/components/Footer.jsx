e1025220c25c323c935a6fad9544a98aaf6d9f7f
ayequill <ayequillgh@gmail.com>
2023-01-09 17:37:40 +0000

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer (){
+    return(
+        <footer>
+            <div className="social-icons">
+            <a target="_blank" href="http://twitter.com/ayequill"><i className="bi bi-twitter"></i></a>
+            <a target="_blank" href="http://facebook.com/ayequill"><i className="bi bi-facebook"></i></a>
+            <a target="_blank" href="http://instagram.com/ayequill"><i className="bi bi-instagram"></i></a>
+            <a target="_blank" href="http://github.com/ayequill"><i className="bi bi-github"></i></a>
+            </div>
+        </footer>
+    )
+}
---
+++ src/components/Info.jsx
@@ -0,0 +1,17 @@
+import siaw from '../assets/siaw.JPG'
+export default function Info (){
+    return(
+        <>
+        <div className="info">
+            <img className='avatar' src={siaw} alt="" />
+            <p className="name">Siaw A. Nicholas</p>
+            <p className="position">Frontend Developer</p>
+            <p className="web">siaw.dev</p>
+            <div className="buttons">
+                <a href='mailto: ayequillgh@gmail.com' className="emailbtn"><i class="bi bi-envelope-at-fill"></i>Email</a>
+                <a target="_blank" href='http://linkedin.com/in/nicholas-siaw-8824a6175/' className="linkbtn"><i class="bi bi-linkedin"></i>Linkedin</a>
+            </div>
+        </div>
+        </>
+    )
+}
